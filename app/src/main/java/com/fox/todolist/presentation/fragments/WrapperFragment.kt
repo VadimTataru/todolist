98ad75dd8b93bb3009793e50410a93cd92259a7a
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-08-09 13:08:03 +0300

    feat: add simple navigation

--- app/src/main/java/com/fox/todolist/presentation/fragments/WrapperFragment.kt
+++ app/src/main/java/com/fox/todolist/presentation/fragments/WrapperFragment.kt
@@ -1,10 +1,12 @@
 package com.fox.todolist.presentation.fragments
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.fox.todolist.R
 import com.fox.todolist.databinding.FragmentWrapperBinding
 
@@ -17,5 +19,12 @@ class WrapperFragment : Fragment() {
     ): View? {
         binding = FragmentWrapperBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_wrapperFragment_to_mainFragment)
+        }, 3000)
     }
 }
