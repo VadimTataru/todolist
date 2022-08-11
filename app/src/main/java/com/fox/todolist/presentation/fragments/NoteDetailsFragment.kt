40dbf72e180e214cb30ea9e257b3ca53dcf24308
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-08-11 11:17:44 +0300

    feat: add date and time pickers

--- app/src/main/java/com/fox/todolist/presentation/fragments/NoteDetailsFragment.kt
+++ app/src/main/java/com/fox/todolist/presentation/fragments/NoteDetailsFragment.kt
@@ -1,15 +1,28 @@
 package com.fox.todolist.presentation.fragments
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import android.widget.TimePicker
 import com.fox.todolist.R
 import com.fox.todolist.databinding.FragmentNoteDetailsBinding
+import java.util.*
 
-class NoteDetailsFragment : Fragment() {
+class NoteDetailsFragment : Fragment(), DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
     lateinit var binding: FragmentNoteDetailsBinding
+
+    private var year = 0
+    private var month = 0
+    private var day = 0
+    private var hour = 0
+    private var minute = 0
+    private var date = ""
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +30,39 @@ class NoteDetailsFragment : Fragment() {
     ): View? {
         binding = FragmentNoteDetailsBinding.inflate(inflater)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnCalendar.setOnClickListener {
+            date = ""
+            pickDate()
+        }
+    }
+
+    private fun pickDate() {
+        getDateTimeCalendar()
+        DatePickerDialog(requireContext(), this, year, month, day).show()
+    }
+
+    private fun getDateTimeCalendar() {
+        val cal = Calendar.getInstance()
+        year = cal.get(Calendar.YEAR)
+        month = cal.get(Calendar.MONTH)
+        day = cal.get(Calendar.DAY_OF_MONTH)
+        hour = cal.get(Calendar.HOUR)
+        minute = cal.get(Calendar.MINUTE)
+    }
+
+    override fun onDateSet(picker: DatePicker?, year: Int, month: Int, day: Int) {
+        date += "$day/${month+1}/$year"
+        getDateTimeCalendar()
+        TimePickerDialog(requireContext(), this, hour, minute, true).show()
+    }
+
+    override fun onTimeSet(picker: TimePicker?, hour: Int, min: Int) {
+        date += " $hour:$min"
+        binding.tvDate.text = ""
+        binding.tvDate.text = date
     }
 }
