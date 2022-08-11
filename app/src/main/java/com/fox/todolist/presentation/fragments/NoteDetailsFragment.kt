4ab64ea12f9d469eb5d47a24b28b76e74e53bc35
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-08-11 10:35:54 +0300

    feat: add note details fragment

---
+++ app/src/main/java/com/fox/todolist/presentation/fragments/NoteDetailsFragment.kt
@@ -0,0 +1,21 @@
+package com.fox.todolist.presentation.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fox.todolist.R
+import com.fox.todolist.databinding.FragmentNoteDetailsBinding
+
+class NoteDetailsFragment : Fragment() {
+    lateinit var binding: FragmentNoteDetailsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentNoteDetailsBinding.inflate(inflater)
+        return binding.root
+    }
+}
