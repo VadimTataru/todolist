215b039bb11cccbb8dbf3c57c1f1174e1f3dd4a1
VadimTataru <64976256+VadimTataru@users.noreply.github.com>
2022-09-06 07:45:33 +0300

    feat: add notification receiver

--- app/src/main/java/com/fox/todolist/utils/Constants.kt
+++ app/src/main/java/com/fox/todolist/utils/Constants.kt
@@ -2,4 +2,10 @@ package com.fox.todolist.utils
 
 object Constants {
     const val CHECK_TAG = "just_checking"
+
+    //notification class
+    const val NOTIFICATION_ID = 1
+    const val CHANNEL_ID = "channel1"
+    const val TITLE_EXTRA = "titleExtra"
+    const val MESSAGE_EXTRA = "messageExtra"
 }
